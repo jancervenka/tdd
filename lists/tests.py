7c794f62e644da4a3b02454632b3ae28d068aa3b
jancervenka <cervej23@fd.cvut.cz>
2017-12-28 07:19:25 +0100

    home_page view function now returns a minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 
@@ -10,3 +11,20 @@ class HomePageTest(TestCase):
         # for root url ('/') resolve django function returns home_page function
         found = resolve('/') 
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        
+        # create dummy http request object
+        request = HttpRequest()
+        
+        # pass the request to the home_page function which returns a response
+        response = home_page(request)
+
+        # extract the html content of the response and convert
+        # the raw bytes to string
+        html = response.content.decode('utf-8')
+
+        # check that the html looks ok
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
-# home_page function created
-def home_page():
-	pass
+# home_page function
+def home_page(request):
+	# return HttpResponse() # return new http response object
+	return HttpResponse('''<html>
+	                           <title>To-Do lists</title>
+	                       </html>''') # better
