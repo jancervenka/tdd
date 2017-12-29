603a6daffda702387707921fbc836ee13d223037
jancervenka <cervej23@fd.cvut.cz>
2017-12-29 03:38:44 +0100

    Functional test now checks we can input an item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -14,7 +16,31 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 
+		# notice the to-do title
 		self.assertIn('To-Do', self.browser.title)
+		# returns one element, raises exception when none is found
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		# invite to enter to-do item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'),
+			             'Enter a to-do item' )
+
+		# type a to-do item into a textbox
+		# Selenium obects have send_keys() to enter string to boxes
+		inputbox.send_keys('Buy peacock feathers')
+
+		# After pressing enter, the page updates a saves the item to 
+		# the list
+		inputbox.send_keys(Keys.ENTER) # press enter
+		time.sleep(1) # wait for the browser to load the page
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr') # elementS - might return an empty list
+
+		# check that the to-do is in the list
+		self.assertTrue(any([row.text == '1: Buy peacock feathers' for row in rows]))
 
 		self.fail('Finish the test!')
 
@@ -22,5 +48,3 @@ if __name__ == '__main__':
 
 	# runs the test
 	unittest.main(warnings = 'ignore')
-
-
