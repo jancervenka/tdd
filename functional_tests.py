1b2825caa76e99da090c6b8c03ca54f9f49f9993
jancervenka <cervej23@fd.cvut.cz>
2018-03-15 01:49:06 +0100

    Refactored functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,13 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	# helper method, does not start with "test"
+	# to remove duplicate code from the functional test method
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr') # elementS - might return an empty list
+		self.assertIn(row_text, [row.text for row in rows])
+
 	# any method with test at the beginning of the name is a test
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
@@ -40,10 +47,12 @@ class NewVisitorTest(unittest.TestCase):
 		rows = table.find_elements_by_tag_name('tr') # elementS - might return an empty list
 
 		# check that the to-do is in the list
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		#self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 		# check for another item
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		#self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 		self.fail('Finish the test!')
 
